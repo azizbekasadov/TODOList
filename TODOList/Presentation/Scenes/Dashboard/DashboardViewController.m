d230b9d8255ce2b8d3c8c4eb478224101c924715
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2025-10-01 13:04:44 +0200

    added create topbar item

--- TODOList/Presentation/Scenes/Dashboard/DashboardViewController.m
+++ TODOList/Presentation/Scenes/Dashboard/DashboardViewController.m
@@ -5,9 +5,14 @@
 //  Created by Azizbek Asadov on 01.10.2025.
 //
 
+@import UIKit;
 #import "DashboardViewController.h"
 
 NS_ASSUME_NONNULL_BEGIN
+
+@interface DashboardViewController()
+
+@end
 
 @implementation DashboardViewController
 
@@ -15,6 +20,17 @@ NS_ASSUME_NONNULL_BEGIN
     [super viewDidLoad];
     
     self.navigationItem.title = @"Dashboard";
+    self.navigationController.navigationBar.prefersLargeTitles = true;
+    
+    if (@available(iOS 16.0, *)) {
+        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Create" image:nil target:self action:@selector(handleCreateRecordPressed) menu:nil];
+    } else {
+        self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Create" style:UIBarButtonItemStylePlain target:self action:@selector(handleCreateRecordPressed)];
+    }
+}
+
+- (void) handleCreateRecordPressed {
+    
 }
 
 @end
